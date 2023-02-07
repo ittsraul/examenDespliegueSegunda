3cfd59c8fe2348e5f97c14fc42ef7a8d54d7886a
ittsraul <raulsahe@protonmail.com>
2023-02-07 13:37:16 +0100

    changing firts

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -10,7 +10,7 @@ class EnanaTest extends TestCase {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida suficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es mayor que 0 y además que su situación es viva
         $enana = new Enana("Arquera",300,"viva");
-        $this->assertEquals(0, $enana->heridaLeve(0));
+        $this->assertEquals("viva", $enana->heridaLeve(0));
     }
 
     /*  public function testHeridaLeveMuere() {
@@ -18,7 +18,7 @@ class EnanaTest extends TestCase {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
         $enana = new Enana("Arquera",10,"muerta");
-        $this->assertEquals("muerta", $enana->heridaLeve("muerta"));
+        $this->assertEquals("0", $enana->heridaLeve(0));
     }
 
     public function testHeridaGrave() {
