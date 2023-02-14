177ff09b959095dd032c71e18c46e83b0584a55d
ittsraul <raulsahe@protonmail.com>
2023-02-14 12:44:41 +0100

    hjjde

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -17,7 +17,7 @@ class EnanaTest extends TestCase {
        
         #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
-        $enana = new Enana("Arquera",10,"viva");
+        $enana = new Enana("Arquera",9,"viva");
         $this->assertEquals("muerta", $enana->heridaLeve());
     }
 
