8e1f56f079f8d198577caaedfa1bdd2f55c1e27e
ittsraul <raulsahe@protonmail.com>
2023-02-14 12:38:35 +0100

    change final

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -17,7 +17,7 @@ class EnanaTest extends TestCase {
        
         #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
-        $enana = new Enana("Arquera",10,"muerta");
+        $enana = new Enana("Arquera",10,"viva");
         $this->assertEquals("muerta", $enana->heridaLeve());
     }
 
@@ -33,7 +33,7 @@ class EnanaTest extends TestCase {
        
         #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
         #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
-        $enana = new Enana("Arquera",0,"muerta");
+        $enana = new Enana("Arquera",-1,"muerta");
         $this->assertEquals("viva", $enana->pocima());
 
     }
@@ -42,7 +42,7 @@ class EnanaTest extends TestCase {
        
         #Se probará el efecto de administrar una pócima Extra a una Enana en el limbo.
         #Se tendrá que probar que la vida es 50 y la situación ha cambiado a viva.
-        $enana = new Enana("Arquera",0,"muerta");
+        $enana = new Enana("Arquera",0,"limbo");
         $this->assertEquals("viva", $enana->pocimaExtra());
 
     }  
